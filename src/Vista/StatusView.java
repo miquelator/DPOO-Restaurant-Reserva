c4a6149f03543b23300d3fc5ff675481faebca82
sartox <ls30927@salleurl.edu>
2018-05-19 16:53:41 +0200

    Modificat els colors de la taula de status segons el seu status.

--- src/Vista/StatusView.java
+++ src/Vista/StatusView.java
@@ -5,6 +5,7 @@ import Controlador.WindowAdapter.StatusWindowClosing;
 import Model.CartaStatus;
 
 import javax.swing.*;
+import javax.swing.table.TableCellRenderer;
 import java.awt.*;
 import java.util.ArrayList;
 
@@ -44,15 +45,23 @@ public class StatusView extends JFrame{
         setContentPane(principal);
     }
 
+    private final Color CONNECTED = new Color(11, 159, 0);
+
+    private Component drawColour(TableCellRenderer renderer, String text){
+        Component component = drawColour(renderer, text);
+        component.setForeground(CONNECTED);
+        return component;
+    }
+
     public void fillComandes (ArrayList<CartaStatus> comanda){
         int size = comanda.size();
         JLabel ordersLabel = new JLabel("", SwingConstants.CENTER);
         String text = "";
         for (int i = 0; i < size; i++){
             if (comanda.get(i).isServit()){
-                text = text + comanda.get(i).getNomPlat() + " --> " + "servit" + "<br/>";
+                text = text + "<font color=green>" + comanda.get(i).getNomPlat() + " --> " + "servit" + "</font>" + "<br/>";
             }else {
-                text = text + comanda.get(i).getNomPlat() + " --> " + "no servit" + "<br/>";
+                text = text + "<font color=red>" + comanda.get(i).getNomPlat() + " --> " + "no servit" + "</font>" + "<br/>";
             }
         }
         text = "<html>" + text + "<html>";
